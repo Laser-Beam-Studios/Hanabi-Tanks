54696dd624f677f80d9e3492cf716ffafff8612b
hotdogwithdog <hotdogwithdog@gmail.com>
2024-12-11 13:50:03 +0100

    Added the java and spring boot

---
+++ hanabi_tanks/src/main/java/laser_beam/hanabi_tanks/HanabiTanksApplication.java
@@ -0,0 +1,13 @@
+package laser_beam.hanabi_tanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanabiTanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanabiTanksApplication.class, args);
+	}
+
+}
---
+++ hanabi_tanks/src/test/java/laser_beam/hanabi_tanks/HanabiTanksApplicationTests.java
@@ -0,0 +1,13 @@
+package laser_beam.hanabi_tanks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanabiTanksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
