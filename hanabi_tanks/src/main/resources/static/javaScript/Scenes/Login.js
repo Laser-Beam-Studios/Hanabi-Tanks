6aa7a8ab773693fc5889f36aafdc20cb274d3e32
hotdogwithdog <hotdogwithdog@gmail.com>
2024-12-14 21:51:21 +0100

    The login now move to the main menu

--- hanabi_tanks/src/main/resources/static/javaScript/Scenes/Login.js
+++ hanabi_tanks/src/main/resources/static/javaScript/Scenes/Login.js
@@ -3,9 +3,6 @@ class Login extends Phaser.Scene
     constructor() 
     {
         super({ key: 'Login' });
-
-        this.musicController;
-        this.songsParts = ["Tanks_Party_A", "Tanks_Party_B", "Tanks_Party_C", "Tanks_Party_D", "Tanks_Party_E"];
     }
 
     preload() 
@@ -13,32 +10,17 @@ class Login extends Phaser.Scene
         this.load.image("MainMenuBackground", "../assets/UI/Screens/mainMenu.png")
 
         this.load.html("LoginDom", "../html/Login.html");
-        
-        // Song
-        this.load.audio("Tanks_Party_A", "../assets/Audio/Music/TanksParty_PART_A.mp3");
-        this.load.audio("Tanks_Party_B", "../assets/Audio/Music/TanksParty_PART_B.mp3");
-        this.load.audio("Tanks_Party_C", "../assets/Audio/Music/TanksParty_PART_C.mp3");
-        this.load.audio("Tanks_Party_D", "../assets/Audio/Music/TanksParty_PART_D.mp3");
-        this.load.audio("Tanks_Party_E", "../assets/Audio/Music/TanksParty_PART_E.mp3");
     }
 
     create() 
     {
-        AudioManager.Instance.SetActiveScene(this);
-
-        if (this.musicController == null)
-        {
-            this.musicController = AudioManager.Instance.CreateInstance("Tanks_Party_A", "Music");
-            this.musicController.Play();
-        }
-        this.musicController.SetCallBack("complete", this.OnMusicPartEnds.bind(this, "Tanks_Party_A"));
-
-
         const Background = this.add.image(WINDOW.WIDHT/2, WINDOW.HEIGHT/2, "MainMenuBackground");
         Scaler.ScaleToGameH(Background);
 
         const login = this.add.dom(WINDOW.WIDHT/2, WINDOW.HEIGHT/2).createFromCache("LoginDom");
         login.addListener("click");
+
+        var scene = this.scene;
 
         login.on("click", function(event)
         {
@@ -61,10 +43,12 @@ class Login extends Phaser.Scene
                         url: USERS_BASE_URL,
                         contentType: "application/json",
                         data: body,
-                        success: (data, status) =>
-                        {
-                            console.log(data);
-                            console.log(status);
+                        success: (data, status) => { 
+                            if (status == "success")
+                                {
+                                    scene.stop("Login");
+                                    scene.start("MainMenu");
+                                }
                         },
                         dataType: "json"
                       });
@@ -72,30 +56,6 @@ class Login extends Phaser.Scene
             }
         });
     }
-
-    OnMusicPartEnds(last)
-    {
-        var lastIdx;
-        for (var i = 0; i < this.songsParts.length; i++)
-        {
-            if(this.songsParts[i] == last) 
-            {
-                lastIdx = i;
-                break;
-            }
-        }
-
-        var randomPartIdx = Math.floor(Math.random() * this.songsParts.length);
-        while(randomPartIdx == lastIdx)
-        {
-            randomPartIdx = Math.floor(Math.random() * this.songsParts.length);
-        }
-        
-        this.musicController = AudioManager.Instance.CreateInstance(this.songsParts[randomPartIdx], "Music");
-        this.musicController.SetCallBack("complete", this.OnMusicPartEnds.bind(this, this.songsParts[randomPartIdx]));
-        this.musicController.Play();
-    }
-
 
     update() 
     {
