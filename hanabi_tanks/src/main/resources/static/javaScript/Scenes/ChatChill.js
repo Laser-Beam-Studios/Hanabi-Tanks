fd517ce2a0a1e7f9bf2a0c943e068d0c50b0bdc2
hotdogwithdog <hotdogwithdog@gmail.com>
2024-12-16 16:02:28 +0100

    Added the conection status to the frontend

--- hanabi_tanks/src/main/resources/static/javaScript/Scenes/ChatChill.js
+++ hanabi_tanks/src/main/resources/static/javaScript/Scenes/ChatChill.js
@@ -6,6 +6,8 @@ class ChatChill extends Phaser.Scene
 
         this.musicController;
         this.songsParts = ["Tanks_Party_A", "Tanks_Party_B", "Tanks_Party_C", "Tanks_Party_D", "Tanks_Party_E"];
+
+        this.connected;
         
         this.lastMessageId = 0;
         this.chatBox;
@@ -25,6 +27,10 @@ class ChatChill extends Phaser.Scene
     {
         this.load.html("ChatDom", "../html/Chat.html");
 
+        // Connected and disconnected UI
+        this.load.image("Connected", "../assets/activo.png");
+        this.load.image("Disconnected", "../assets/desactivo.png");
+
         // Song
         this.load.audio("Tanks_Party_A", "../assets/Audio/Music/TanksParty_PART_A.mp3");
         this.load.audio("Tanks_Party_B", "../assets/Audio/Music/TanksParty_PART_B.mp3");
@@ -35,14 +41,27 @@ class ChatChill extends Phaser.Scene
 
     create()
     {
+        // AUDIO
         AudioManager.Instance.SetActiveScene(this);
 
         this.musicController = AudioManager.Instance.CreateInstance("Tanks_Party_A", "Music");
         this.musicController.Play();
         this.musicController.SetCallBack("complete", this.OnMusicPartEnds.bind(this, "Tanks_Party_A"));
 
+        // UI
+        this.connected = this.add.image((WINDOW.WIDHT * 13)/32, WINDOW.HEIGHT * 0.97, "Connected");
+        Scaler.ScaleToGameW(this.connected, 0.04);
+        this.disconnected = this.add.image((WINDOW.WIDHT * 13)/32, WINDOW.HEIGHT * 0.97, "Disconnected");
+        Scaler.ScaleToGameW(this.disconnected, 0.04);
+
+        this.connected.visible = false;
+        this.disconnected.visible = false;
+
+
+        // INPUTS
         this.input.keyboard.on("keydown", this.OnKeyPressed.bind(this));
 
+        // CHAT
         this.lastMessageId = 0;
         this.chatBoxDomElement = this.add.dom(0, WINDOW.HEIGHT - 100).createFromCache("ChatDom");
         this.chatBoxDomElement.alpha = 0.4;
@@ -52,6 +71,10 @@ class ChatChill extends Phaser.Scene
         this.messages = $("#messages");
 
         setInterval(this.UpdateChat, 2000, this);
+        setInterval(this.UpdateUsersConnected, 2000, this);
+
+        this.UpdateChat(this);  // For fecth the messages at the start
+        this.UpdateUsersConnected(this) // The same reason but with the logos of conneted or disconnected
     }
 
     SendMessage(username)
@@ -77,7 +100,7 @@ class ChatChill extends Phaser.Scene
 
         $.get(CHAT_BASE_URL, { since: THIS.lastMessageId, username: THIS.username }, (data) =>
         {
-            console.log("The get returns: " + data.lastId);
+            console.log("last message ID: " + data.lastId);
             if (data.messages && data.messages.length > 0 && THIS.lastMessageId <= data.lastId) {
                 for (var i = 0; i < data.messages.length; i++)
                 {
@@ -86,6 +109,31 @@ class ChatChill extends Phaser.Scene
                 THIS.messages.scrollTop(THIS.messages.prop("scrollHeight"));
                 THIS.lastMessageId = data.lastId + 1;    // The + 1 it's for the server api, it returns the actual index of the message that it's now display in the chatBox so the next time i get the nexts
             }
+        });
+    }
+
+    UpdateUsersConnected(THIS)
+    {
+        console.log("Updating users connected...");
+
+        $.get(USERS_BASE_URL + "/connected", {}, (connected) =>
+        {
+            console.log("Connected Users: " + connected);
+            if (connected > 0)
+            {
+                THIS.connected.visible = true;
+                THIS.disconnected.visible = false;
+            }
+            else
+            {
+                THIS.connected.visible = false;
+                THIS.disconnected.visible = true;
+            }
+        }).error(() =>
+        {
+            console.log("ERROR IN GET");
+            THIS.connected.visible = false;
+            THIS.disconnected.visible = true;
         });
     }
 
